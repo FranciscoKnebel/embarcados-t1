77392c9d5a8d1c99a4f1644a8d7ec53e5b2aac89
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-10-06 20:00:04 -0300

    vai papai

--- goertzel/gortzelfilter.c
+++ goertzel/gortzelfilter.c
@@ -1,11 +1,51 @@
+/* goertzelfilter.c
+*/
+
 #include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <complex.h>
+
 
 #include "goertzelfilter.h"
 
+
 #define LEN(a) (sizeof(a)/sizeof(a[0]) )
+
+GoertzelFilter goertzelSetup( double normalizedfreq )
+{
+  double w = 2*M_PI*normalizedfreq;
+  double wr, wi;
+
+  GoertzelFilter g;
+
+  wr = cos(w);
+  wi = sin(w);
+  g.coeff = 2 * wr;
+  g.cosine = wr;
+  g.sine = wi;
+
+  return g;
+}
+
+double complex goertzelFilterC( double *samples, int nsamples, GoertzelFilter *g )
+{
+  double sprev = 0.0;
+  double sprev2 = 0.0;
+  double s, imag, real;
+  int n;
+
+  for (n=0; n<nsamples; n++ ) {
+    s = samples[n] + g->coeff * sprev - sprev2;
+    sprev2 = sprev;
+    sprev = s;
+  }
+
+  real = sprev*g->cosine - sprev2;
+  imag = -sprev*g->sine;
+
+  return real + I*imag;
+}
 
 int main() {
 
