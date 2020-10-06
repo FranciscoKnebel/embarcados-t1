fef4b463a6eb0d9af09c913c1d46f2a02b182409
predowtf <pedropedra@hotmail.com>
2020-10-06 19:58:55 -0300

    Update goertzefilter.c
    Juntei o main no mesmo arquivo

--- goertzel/goertzefilter.c
+++ goertzel/goertzefilter.c
@@ -3,9 +3,14 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <complex.h>
 
+
 #include "goertzelfilter.h"
+
+
+#define LEN(a) (sizeof(a)/sizeof(a[0]) )
 
 GoertzelFilter goertzelSetup( double normalizedfreq )
 {
@@ -42,3 +47,36 @@ double complex goertzelFilterC( double *samples, int nsamples, GoertzelFilter *g
   return real + I*imag;
 }
 
+int main() {
+
+  // This will hold our input and output data
+  double data[1024] = { 0. };
+  double complex filtered[1024] = { 0. };
+
+  // This will hold the filter constants
+  GoertzelFilter g = { 0. };
+
+  int n;
+  int nwindow = 16;
+
+  double f = 4./LEN(data) ;
+
+  // Generate data with noise
+  for ( n = 0 ; n < LEN(data) ; n++ ) {
+    data[n] = sin( n * (2.*M_PI) * f ) + 0.5*((float)rand()/RAND_MAX - 0.5);
+  }
+
+  // Set up the filter constants, note that we choose a frequency
+  g = goertzelSetup( f );
+
+  // Filter the data using a sliding window
+  for( n = 0 ; n < LEN(data)-nwindow ; n++ ) {
+    filtered[n+nwindow/2]  = goertzelFilterC( &data[n], nwindow, &g )/nwindow;
+  }
+
+  // Print the real Valued Data (1st column) and complex valued Goertzel output
+  for( n = 0 ; n < LEN(data); n++ ) {
+    printf( "%g %g %g\n", data[n], creal(filtered[n]), cimag(filtered[n]) );
+  }
+
+}
